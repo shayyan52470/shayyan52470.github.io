a51978e0335130ea89ae87cabb285a0e401c150b
shayyan52470 <38004914+shayyan52470@users.noreply.github.com>
2023-01-01 19:22:06 +1100

    Uploaded both helloworld.js & index.HTML files
    Added these two files to test everything and see if they work

---
+++ helloworld.js
@@ -0,0 +1,11 @@
+console.log('Hello, World!')
+console.log(true) //We have booleans, yay!
+console.log(typeof "Hello, is it me you're looking for") // Prints as string
+let firstName = "Shayyan"
+console.log(firstName)
+/* Multiline comments
+Let's go!
+*/
+let lastName = "Ali"
+let age = 18
+console.log(firstName,lastName,"Age:",age)
